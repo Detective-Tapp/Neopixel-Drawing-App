b2e6c20a28f32f722642b85e88bc268a62c43ba3
Detective-Tapp <burwessel@gmail.com>
2022-02-09 10:21:19 +0100

    Add project files.

---
+++ Learning to draw/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Learning_to_draw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Draw = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.export = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.exportColor = new System.Windows.Forms.TextBox();
+            this.redSlider = new System.Windows.Forms.TrackBar();
+            this.greenSlider = new System.Windows.Forms.TrackBar();
+            this.blueSlider = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.colorPreview = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Draw
+            // 
+            this.Draw.Location = new System.Drawing.Point(44, 16);
+            this.Draw.Margin = new System.Windows.Forms.Padding(4);
+            this.Draw.Name = "Draw";
+            this.Draw.Size = new System.Drawing.Size(100, 28);
+            this.Draw.TabIndex = 0;
+            this.Draw.Text = "Draw";
+            this.Draw.UseVisualStyleBackColor = true;
+            this.Draw.Click += new System.EventHandler(this.Draw_Click_1);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(153, 16);
+            this.Clear.Margin = new System.Windows.Forms.Padding(4);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(100, 28);
+            this.Clear.TabIndex = 1;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // export
+            // 
+            this.export.Location = new System.Drawing.Point(263, 16);
+            this.export.Margin = new System.Windows.Forms.Padding(4);
+            this.export.Name = "export";
+            this.export.Size = new System.Drawing.Size(100, 28);
+            this.export.TabIndex = 2;
+            this.export.Text = "export";
+            this.export.UseVisualStyleBackColor = true;
+            this.export.Click += new System.EventHandler(this.export_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(44, 53);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(933, 492);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(371, 24);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(478, 24);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 22);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            // 
+            // exportColor
+            // 
+            this.exportColor.Location = new System.Drawing.Point(559, 24);
+            this.exportColor.Name = "exportColor";
+            this.exportColor.Size = new System.Drawing.Size(100, 22);
+            this.exportColor.TabIndex = 6;
+            this.exportColor.Click += new System.EventHandler(this.exportColor_Click);
+            // 
+            // redSlider
+            // 
+            this.redSlider.Location = new System.Drawing.Point(984, 16);
+            this.redSlider.Maximum = 255;
+            this.redSlider.Name = "redSlider";
+            this.redSlider.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.redSlider.Size = new System.Drawing.Size(56, 529);
+            this.redSlider.TabIndex = 7;
+            this.redSlider.ValueChanged += new System.EventHandler(this.redSlider_ValueChanged);
+            // 
+            // greenSlider
+            // 
+            this.greenSlider.Location = new System.Drawing.Point(1046, 16);
+            this.greenSlider.Maximum = 255;
+            this.greenSlider.Name = "greenSlider";
+            this.greenSlider.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.greenSlider.Size = new System.Drawing.Size(56, 529);
+            this.greenSlider.TabIndex = 8;
+            this.greenSlider.Scroll += new System.EventHandler(this.greenSlider_Scroll);
+            // 
+            // blueSlider
+            // 
+            this.blueSlider.Location = new System.Drawing.Point(1108, 16);
+            this.blueSlider.Maximum = 255;
+            this.blueSlider.Name = "blueSlider";
+            this.blueSlider.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.blueSlider.Size = new System.Drawing.Size(56, 529);
+            this.blueSlider.TabIndex = 9;
+            this.blueSlider.Scroll += new System.EventHandler(this.blueSlider_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(371, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "light array";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(478, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "led amout";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(559, 1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "led color array";
+            // 
+            // colorPreview
+            // 
+            this.colorPreview.Location = new System.Drawing.Point(666, 16);
+            this.colorPreview.Name = "colorPreview";
+            this.colorPreview.Size = new System.Drawing.Size(311, 30);
+            this.colorPreview.TabIndex = 13;
+            this.colorPreview.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1177, 554);
+            this.Controls.Add(this.colorPreview);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.blueSlider);
+            this.Controls.Add(this.greenSlider);
+            this.Controls.Add(this.redSlider);
+            this.Controls.Add(this.exportColor);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.export);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Draw);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Draw;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button export;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox exportColor;
+        private System.Windows.Forms.TrackBar redSlider;
+        private System.Windows.Forms.TrackBar greenSlider;
+        private System.Windows.Forms.TrackBar blueSlider;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox colorPreview;
+    }
+}
+
---
+++ Learning to draw/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Learning_to_draw
+{
+    public partial class Form1 : Form
+    {
+        int[] exportLeds = new int[0];
+        int[] exportColour = new int[0];
+
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+
+        int newRow = 0;
+
+
+        bool[,] matrix = new bool[40,16];
+        public Form1()
+        {
+            
+            InitializeComponent();
+            DrawGrid();
+        }
+        public void DrawGrid()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            int xBound = pictureBox1.Width;
+            int yBound = pictureBox1.Height;
+
+            int i = 0;
+            int j;
+
+            using (Graphics gr = Graphics.FromImage(bmp))
+            {
+                // Create pen.
+                Pen blackPen = new Pen(Color.Black, 1);
+
+                for (i = 0; i < 40; i++)
+                {
+                    for (j = 0; j < 15; j++)
+                    {
+                        Rectangle columns = new Rectangle(i*15, j*15, 10, 10);
+                        gr.DrawRectangle(blackPen, columns);
+                    }
+
+                    Rectangle rows = new Rectangle(i*15, j*15, 10, 10);
+                    gr.DrawRectangle(blackPen, rows);
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+        private void Draw_Click_1(object sender, EventArgs e)
+        {
+            //DrawRectangleRectangle();
+            DrawGrid();
+        }
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            //Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            //pictureBox1.Image = bmp;
+            DrawGrid();
+            for (int i = 0; i < 40; i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    matrix[i,j] = false;
+                }
+            }
+            exportColour = new int[0];
+            exportLeds = new int[0];
+        }
+        void pictureBox1_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+            using (Graphics gr = Graphics.FromImage(bmp))
+            {
+                SolidBrush blueBrush = new SolidBrush(Color.FromArgb(red,green,blue));
+                SolidBrush erasor = new SolidBrush(pictureBox1.BackColor);
+                Pen blackPen = new Pen(Color.Black, 10);
+                for (int i = 0; i < 40; i++)
+                {
+                    for (int j = 0; j < 16; j++)
+                    {
+                        if (e.X > (i * 15) -2.5f && e.X < i * 15 + 12.5f)
+                        {
+                            if (e.Y > (j * 15) -2.5f && e.Y < j * 15 + 12.5f)
+                            {
+                                if (matrix[i,j] == false)
+                                {
+                                    if (i > 7 && i < 16)
+                                        newRow = 120;
+                                    if (i > 15 && i < 24)
+                                        newRow = 240;
+                                    if (i > 23 && i < 32)
+                                        newRow = 360;
+                                    if (i > 31 && i < 40)
+                                        newRow = 480;
+
+                                    Rectangle rect = new Rectangle(i * 15 +1, j * 15 +1, 9, 9);
+                                    gr.FillRectangle(blueBrush, rect);
+                                    matrix[i, j] = true;
+
+                                    Array.Resize(ref exportLeds, exportLeds.Length + 1);
+                                    exportLeds[exportLeds.Length - 1] = (i + newRow) + ((j * 8) + 1);
+                                    Array.Resize(ref exportColour, exportColour.Length +3); 
+                                    exportColour[exportColour.Length - 3] = red;
+                                    exportColour[exportColour.Length - 2] = green;
+                                    exportColour[exportColour.Length - 1] = blue;
+                                }
+                                else
+                                {
+                                    Rectangle rect = new Rectangle(i * 15 +1, j * 15 +1, 9, 9);
+                                    gr.FillRectangle(erasor, rect);
+                                    matrix[i, j] = false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+        private void export_Click(object sender, EventArgs e)
+        {
+            calledByExportArray();
+            calledByTextBox2();
+            calledByExportColor();
+        }
+        void calledByExportArray()
+        {
+            string output = "{";
+            string[] stringArray = new string[exportLeds.Length];
+            stringArray = Array.ConvertAll(exportLeds, x => x.ToString());
+            for (int x = 0; x < stringArray.Length; x++)
+            {
+                output = output + stringArray[x] + ',';
+            }
+            output = output.Remove(output.Length - 1, 1);
+            output = output + '}';
+            textBox1.Text = output;
+            output = "";
+        }
+        void calledByExportColor()
+        {
+            
+            /*
+            int count = 0;
+            for (int i = 0; i < 40; i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    if (matrix[i, j])
+                        count++;
+                }
+            }
+            int[] export = new int[count * 3];
+            for (int i = 0; i < export.Length; i++)
+            {
+                export[i] = red;
+                export[i + 1] = green;
+                export[i + 2] = blue;
+                i++;
+                i++;
+            }
+            */
+
+            string output = "{";
+            string[] stringArray = new string[exportColour.Length];
+            stringArray = Array.ConvertAll(exportColour, x => x.ToString());
+            for (int x = 0; x < stringArray.Length; x++)
+            {
+                output = output + stringArray[x] + ',';
+            }
+            output = output.Remove(output.Length - 1, 1);
+            output = output + '}';
+            exportColor.Text = output;
+            output = "";
+        }
+        void calledByTextBox2() {
+            int count = 0;
+            for (int i = 0; i < 40; i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    if (matrix[i, j])
+                        count++;
+                }
+            }
+            textBox2.Text = count.ToString();
+        }
+        void calledByExport(int count)
+        {
+            int i;
+            int j;
+
+            int newRow = 0;
+
+            for (i = 0; i < 40; i++)
+            {
+                for (j = 0; j < 16; j++)
+                {
+                    
+                    if (matrix[i, j])
+                    {
+                        if (i > 7 && i < 16)
+                            newRow = 120;
+                        if (i > 15 && i < 24)
+                            newRow = 240;
+                        if (i > 23 && i < 32)
+                            newRow = 360;
+                        if (i > 31 && i < 40)
+                            newRow = 480;
+
+                        if (count > 0)
+                        {
+                            if (exportLeds[count - 1] != (i + newRow) + ((j * 8) + 1))
+                            {
+                                exportLeds[count] = (i + newRow) + ((j * 8) + 1);
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            exportLeds[count] = (i + newRow) + ((j * 8) + 1);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            calledByTextBox2();
+        }
+        private void redSlider_ValueChanged(object sender, EventArgs e)
+        {
+            red = redSlider.Value;
+            colorPreview.BackColor = Color.FromArgb(red, green, blue);
+
+        }
+        private void greenSlider_Scroll(object sender, EventArgs e)
+        {
+            green = greenSlider.Value;
+            colorPreview.BackColor = Color.FromArgb(red,green,blue);
+        }
+        private void blueSlider_Scroll(object sender, EventArgs e)
+        {
+            blue = blueSlider.Value;
+            colorPreview.BackColor = Color.FromArgb(red, green, blue);
+        }
+        private void exportColor_Click(object sender, EventArgs e)
+        {
+            calledByExportColor();
+        }
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            /*
+            int[] export = new int[(40 * 16)];
+
+            int i;
+            int j;
+
+            int newRow = 0;
+
+            int count = 0;
+
+            for (i = 0; i < 40; i++)
+            {
+                for (j = 0; j < 16; j++)
+                {
+                    if (matrix[i, j])
+                    {
+                        if (i > 7 && i < 16)
+                            newRow = 120;
+                        if (i > 15 && i < 24)
+                            newRow = 240;
+                        if (i > 23 && i < 32)
+                            newRow = 360;
+                        if (i > 31 && i < 40)
+                            newRow = 480;
+
+                        export[count] = (i + newRow) + ((j * 8) + 1);
+                        count++;
+                    }
+                }
+            }
+            Array.Sort(export);
+            string output = "{";
+            string[] stringArray = new string[40 * 16];
+            stringArray = Array.ConvertAll(export, x => x.ToString());
+            for (int x = 0; x < stringArray.Length; x++)
+            {
+                if (stringArray[x] != "0")
+                {
+                    output = output + stringArray[x] + ',';
+                }
+            }
+            output = output.Remove(output.Length - 1, 1);
+            output = output + '}';
+            textBox1.Text = output;
+            output = "";
+            */
+        }
+    }
+}
